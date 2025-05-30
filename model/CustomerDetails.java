56040dce8ed218707b7a72ebb7483de9ce4700c6
AzriHarniza22 <azri.22@mhs.usk.ac.id>
2025-05-30 20:24:05 +0700

    revisi

--- model/CustomerDetails.java
+++ model/CustomerDetails.java
@@ -4,8 +4,6 @@ public class CustomerDetails {
     public String customerId;
     public String name;
     public String email;
-    public String phone;
-    public String address;
 
     public CustomerDetails(String customerId, String name, String email) {
         this.customerId = customerId;
@@ -17,7 +15,5 @@ public class CustomerDetails {
         this.customerId = customerId;
         this.name = name;
         this.email = email;
-        this.phone = phone;
-        this.address = address;
     }
 }
